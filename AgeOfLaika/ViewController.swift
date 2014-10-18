886cb88e7a121328a0a2154cb5b1b6b974c465f6
Dewey Kang <hello@deweykang.com>
2014-10-18 08:57:21 -0400

    completed assignment

--- AgeOfLaika/ViewController.swift
+++ AgeOfLaika/ViewController.swift
@@ -10,6 +10,11 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    @IBOutlet weak var convertedTextDogYears: UITextField!
+    @IBOutlet weak var enterTextHumanYears: UITextField!
+    
+    @IBOutlet weak var conversionButton: UIButton!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
@@ -19,7 +24,20 @@ class ViewController: UIViewController {
         super.didReceiveMemoryWarning()
         // Dispose of any resources that can be recreated.
     }
-
+    
+    @IBAction func convertToDogYears(sender: UIButton) {
+       
+        let humanYearsFromTextField = enterTextHumanYears.text.toInt()!
+        
+        enterTextHumanYears.text = ""
+        enterTextHumanYears.resignFirstResponder()
+        
+        conversionButton.setTitle("Converted!", forState: UIControlState.Normal)
+        
+        convertedTextDogYears.hidden = false
+        convertedTextDogYears.text = " \(humanYearsFromTextField * 7)" + " Human Years!"
+        
+    }
 
 }
 
